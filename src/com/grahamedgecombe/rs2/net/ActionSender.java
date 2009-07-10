0fa2d207a2fb752920d35427cc9fbdacf35775e3
Graham Edgecombe <graham@grahamedgecombe.com>
2009-07-10 16:16:01 +0000

    - Added decoder.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@10 c48d7634-943c-464e-a392-e2f9434beeda

--- src/com/grahamedgecombe/rs2/net/ActionSender.java
+++ src/com/grahamedgecombe/rs2/net/ActionSender.java
@@ -1,5 +1,8 @@
 package com.grahamedgecombe.rs2.net;
 
+import org.apache.mina.core.future.IoFuture;
+import org.apache.mina.core.future.IoFutureListener;
+
 import com.grahamedgecombe.rs2.model.Player;
 import com.grahamedgecombe.rs2.net.Packet.Type;
 
@@ -18,6 +21,15 @@ public class ActionSender {
 	
 	public void sendMessage(String message) {
 		player.getSession().write(new PacketBuilder(253, Type.VARIABLE).putRS2String(message).toPacket());
+	}
+	
+	public void sendLogout() {
+		player.getSession().write(new PacketBuilder(109).toPacket()).addListener(new IoFutureListener<IoFuture>() {
+			@Override
+			public void operationComplete(IoFuture future) {
+				future.getSession().close(false);
+			}
+		});
 	}
 
 }
--- src/com/grahamedgecombe/rs2/net/RS2Decoder.java
+++ src/com/grahamedgecombe/rs2/net/RS2Decoder.java
@@ -5,12 +5,49 @@ import org.apache.mina.core.session.IoSession;
 import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
 import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 
+import com.grahamedgecombe.rs2.Constants;
+import com.grahamedgecombe.rs2.model.Player;
+import com.grahamedgecombe.rs2.net.Packet.Type;
+
 public class RS2Decoder extends CumulativeProtocolDecoder {
 
 	@Override
 	protected boolean doDecode(IoSession session, IoBuffer in, ProtocolDecoderOutput out) throws Exception {
 		synchronized(session) {
+			ISAACCipher inCipher = ((Player) session.getAttribute("player")).getInCipher();
 			
+			int opcode = (Integer) session.getAttribute("opcode", -1);
+			int size = (Integer) session.getAttribute("size", -1);
+			if(opcode == -1) {
+				if(in.remaining() >= 1) {
+					opcode = in.get() & 0xFF;
+					opcode = (opcode - inCipher.getNextKey()) & 0xFF;
+					size = Constants.PACKET_SIZES[opcode];
+					session.setAttribute("opcode", opcode);
+					session.setAttribute("size", size);
+				} else {
+					return false;
+				}
+			}
+			if(size == -1) {
+				if(in.remaining() >= 1) {
+					size = in.get() & 0xFF;
+					session.setAttribute("size", size);
+				} else {
+					return false;
+				}
+			}
+			if(in.remaining() >= size) {
+				byte[] data = new byte[size];
+				in.get(data);
+				IoBuffer payload = IoBuffer.allocate(data.length);
+				payload.put(data);
+				payload.flip();
+				out.write(new Packet(opcode, Type.FIXED, payload));
+				session.setAttribute("opcode", -1);
+				session.setAttribute("size", -1);
+				return true;
+			}
 			return false;
 		}
 	}
