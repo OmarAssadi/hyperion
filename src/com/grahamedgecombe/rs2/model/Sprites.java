8e0ea0735f01386f0fc5e61d8f66e0630cc50a9e
Graham Edgecombe <graham@grahamedgecombe.com>
2009-07-11 16:41:36 +0000

    - Finished sprites class.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@37 c48d7634-943c-464e-a392-e2f9434beeda

--- src/com/grahamedgecombe/rs2/model/Sprites.java
+++ src/com/grahamedgecombe/rs2/model/Sprites.java
@@ -2,12 +2,25 @@ package com.grahamedgecombe.rs2.model;
 
 public class Sprites {
 	
+	private int primary = -1;
+	private int secondary = -1;
+	
 	public int getPrimarySprite() {
-		return -1;
+		return primary;
 	}
 	
 	public int getSecondarySprite() {
-		return -1;
+		return secondary;
+	}
+	
+	public void setSprite(int sprite) {
+		primary = sprite;
+		secondary = -1;
+	}
+	
+	public void setSprites(int primary, int secondary) {
+		this.primary = primary;
+		this.secondary = secondary;
 	}
 
 }
