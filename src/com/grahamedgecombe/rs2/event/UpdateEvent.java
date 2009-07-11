e9e9faa249ebc2813eed94760e8b289b2891dc0b
Graham Edgecombe <graham@grahamedgecombe.com>
2009-07-11 14:55:49 +0000

    - UpdateEvent now submits a ParallelTask of UpdateTasks.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@29 c48d7634-943c-464e-a392-e2f9434beeda

--- src/com/grahamedgecombe/rs2/event/UpdateEvent.java
+++ src/com/grahamedgecombe/rs2/event/UpdateEvent.java
@@ -1,5 +1,14 @@
 package com.grahamedgecombe.rs2.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.grahamedgecombe.rs2.model.Player;
+import com.grahamedgecombe.rs2.model.World;
+import com.grahamedgecombe.rs2.task.ParallelTask;
+import com.grahamedgecombe.rs2.task.Task;
+import com.grahamedgecombe.rs2.task.UpdateTask;
+
 public class UpdateEvent extends Event {
 
 	public static final int CYCLE_TIME = 600;
@@ -10,7 +19,11 @@ public class UpdateEvent extends Event {
 
 	@Override
 	public void execute() {
-		
+		List<Task> tasks = new ArrayList<Task>();
+		for(Player player : World.getWorld().getPlayers()) {
+			tasks.add(new UpdateTask(player));
+		}
+		World.getWorld().getEngine().pushTask(new ParallelTask(tasks.toArray(new Task[0])));
 	}
 
 }
