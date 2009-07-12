4fca99d8f7247b89f99a64231a21f0784cd7d7ab
Graham Edgecombe <graham@grahamedgecombe.com>
2009-07-12 15:55:37 +0000

    - Fix to large local lists.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@67 c48d7634-943c-464e-a392-e2f9434beeda

--- src/com/grahamedgecombe/rs2/task/UpdateTask.java
+++ src/com/grahamedgecombe/rs2/task/UpdateTask.java
@@ -123,32 +123,38 @@ public class UpdateTask implements Task {
 		/*
 		 * Loop through every player.
 		 */
-		for(Player otherPlayer : World.getWorld().getPlayers()) {
+		if(player.getLocalPlayers().size() < 256) {
 			/*
-			 * If they should not be added ignore them.
+			 * We have room in the list (the list can only store 255 players
+			 * maximum as the size is sent in 8 bits).
 			 */
-			if(otherPlayer == player || player.getLocalPlayers().contains(otherPlayer)) {
-				continue;
-			}
-			
-			/*
-			 * If the player could be added, check if it is within distance.
-			 */
-			if(otherPlayer.getLocation().isWithinDistance(player.getLocation())) {
+			for(Player otherPlayer : World.getWorld().getPlayers()) {
 				/*
-				 * Add the player to the local list if it is within distance.
+				 * If they should not be added ignore them.
 				 */
-				player.getLocalPlayers().add(otherPlayer);
+				if(otherPlayer == player || player.getLocalPlayers().contains(otherPlayer)) {
+					continue;
+				}
 				
 				/*
-				 * Add the player in the packet.
+				 * If the player could be added, check if it is within distance.
 				 */
-				addNewPlayer(packet, otherPlayer);
-				
-				/*
-				 * Update the player, forcing the appearance flag.
-				 */
-				updatePlayer(updateBlock, otherPlayer, true);
+				if(otherPlayer.getLocation().isWithinDistance(player.getLocation())) {
+					/*
+					 * Add the player to the local list if it is within distance.
+					 */
+					player.getLocalPlayers().add(otherPlayer);
+					
+					/*
+					 * Add the player in the packet.
+					 */
+					addNewPlayer(packet, otherPlayer);
+					
+					/*
+					 * Update the player, forcing the appearance flag.
+					 */
+					updatePlayer(updateBlock, otherPlayer, true);
+				}
 			}
 		}
 		
