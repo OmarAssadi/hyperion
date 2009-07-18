28d7cfb8554970fab0d4fa334fb9b066a79d1fdc
Graham Edgecombe <graham@grahamedgecombe.com>
2009-07-18 08:05:36 +0000

    - Commented login as it's the last big feature to comment which was left out.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@97 c48d7634-943c-464e-a392-e2f9434beeda

--- src/org/hyperion/rs2/net/RS2LoginDecoder.java
+++ src/org/hyperion/rs2/net/RS2LoginDecoder.java
@@ -70,6 +70,16 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 			switch(state) {
 			case STATE_OPCODE:
 				if(in.remaining() >= 1) {
+					/*
+					 * Here we read the first opcode which indicates the type
+					 * of connection.
+					 * 
+					 * 14 = game
+					 * 15 = update
+					 * 
+					 * Updating is disabled in the vast majority of 317
+					 * clients.
+					 */
 					int opcode = in.get() & 0xFF;
 					switch(opcode) {
 					case OPCODE_GAME:
@@ -87,9 +97,24 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 				break;
 			case STATE_LOGIN:
 				if(in.remaining() >= 1) {
+					/*
+					 * The name hash is a simple hash of the name which is
+					 * suspected to be used to select the appropriate login
+					 * server.
+					 */
 					@SuppressWarnings("unused")
 					int nameHash = in.get() & 0xFF;
+					
+					/*
+					 * We generated the server session key using a SecureRandom
+					 * class for security.
+					 */
 					long serverKey = RANDOM.nextLong();
+					
+					/*
+					 * The initial response is just 0s which the client is set
+					 * to ignore (probably some sort of modification).
+					 */
 					session.write(new PacketBuilder().put(INITIAL_RESPONSE).put((byte) 0).putLong(serverKey).toPacket());
 					session.setAttribute("state", STATE_PRECRYPTED);
 					session.setAttribute("serverKey", serverKey);
@@ -98,6 +123,12 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 				break;
 			case STATE_PRECRYPTED:
 				if(in.remaining() >= 2) {
+					/*
+					 * We read the type of login.
+					 * 
+					 * 16 = normal
+					 * 18 = reconnection
+					 */
 					int loginOpcode = in.get() & 0xFF;
 					if(loginOpcode != 16 && loginOpcode != 18) {
 						logger.info("Invalid login opcode : " + loginOpcode);
@@ -105,8 +136,20 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						in.rewind();
 						return false;
 					}
+					
+					/*
+					 * We read the size of the login packet.
+					 */
 					int loginSize = in.get() & 0xFF;
+					
+					/*
+					 * And calculated how long the encrypted block will be.
+					 */
 					int loginEncryptSize = loginSize - (36 + 1 + 1 + 2);
+					
+					/*
+					 * This could be invalid so if it is we ignore it.
+					 */
 					if(loginEncryptSize <= 0) {
 						logger.info("Encrypted packet size zero or negative : " + loginEncryptSize);
 						session.close(false);
@@ -123,6 +166,10 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 				int size = (Integer) session.getAttribute("size");
 				int encryptSize = (Integer) session.getAttribute("encryptSize");
 				if(in.remaining() >= size) {
+					/*
+					 * We read the magic ID which is 255 (0xFF) which indicates
+					 * this is the real login packet.
+					 */
 					int magicId = in.get() & 0xFF;
 					if(magicId != 255) {
 						logger.info("Incorrect magic id : " + magicId);
@@ -130,6 +177,11 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						in.rewind();
 						return false;
 					}
+					
+					/*
+					 * We now read a short which is the client version and
+					 * check if it equals 317.
+					 */
 					int version = in.getShort() & 0xFFFF;
 					if(version != 317) {
 						logger.info("Incorrect version : " + version);
@@ -137,12 +189,30 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						in.rewind();
 						return false;
 					}
+					
+					/*
+					 * The following byte indicates if we are using a low
+					 * memory version.
+					 */
 					@SuppressWarnings("unused")
 					boolean lowMemoryVersion = (in.get() & 0xFF) == 1;
+					
+					/*
+					 * We know read the cache indices.
+					 */
 					for(int i = 0; i < 9; i++) {
 						in.getInt();
 					}
+					
+					/*
+					 * The encrypted size includes the size byte which we don't
+					 * need.
+					 */
 					encryptSize--;
+					
+					/*
+					 * We check if there is a mismatch in the sizing.
+					 */
 					int reportedSize = in.get() & 0xFF;
 					if(reportedSize != encryptSize) {
 						logger.info("Packet size mismatch (expected : " + encryptSize + ", reported : " + reportedSize + ")");
@@ -150,6 +220,12 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						in.rewind();
 						return false;
 					}
+					
+					/*
+					 * We now read the encrypted block opcode (although in most
+					 * 317 clients and this server the RSA is disabled) and
+					 * check it is equal to 10.
+					 */
 					int blockOpcode = in.get() & 0xFF;
 					if(blockOpcode != 10) {
 						logger.info("Invalid login block opcode : " + blockOpcode);
@@ -158,7 +234,14 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						return false;
 					}
 	
+					/*
+					 * We read the client's session key.
+					 */
 					long clientKey = in.getLong();
+					
+					/*
+					 * And verify it has the correct server session key.
+					 */
 					long serverKey = (Long) session.getAttribute("serverKey");
 					long reportedServerKey = in.getLong();
 					if(reportedServerKey != serverKey) {
@@ -167,11 +250,30 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 						in.rewind();
 						return false;
 					}
+					
+					/*
+					 * The UID, found in random.dat in newer clients and
+					 * uid.dat in older clients is a way of identifying a
+					 * computer.
+					 * 
+					 * However, some clients send a hardcoded or random UID,
+					 * making it useless in the private server scene.
+					 */
 					int uid = in.getInt();
+					
+					/*
+					 * We read and format the name and passwords.
+					 */
 					String name = NameUtils.formatName(IoBufferUtils.getRS2String(in));
 					String pass = IoBufferUtils.getRS2String(in);
 					logger.info("Login request : username=" + name + " password=" + pass);
 					
+					/*
+					 * And setup the ISAAC cipher which is used to encrypt and
+					 * decrypt opcodes.
+					 * 
+					 * However, without RSA, this is rendered useless anyway.
+					 */
 					int[] sessionKey = new int[4];
 					sessionKey[0] = (int) (clientKey >> 32);
 					sessionKey[1] = (int) clientKey;
@@ -189,6 +291,11 @@ public class RS2LoginDecoder extends CumulativeProtocolDecoder {
 					}
 					ISAACCipher outCipher = new ISAACCipher(sessionKey);
 					
+					/*
+					 * Now, the login has completed, and we do the appropriate
+					 * things to fire off the chain of events which will load
+					 * and check the saved games etc.
+					 */
 					session.getFilterChain().remove("protocol");
 					session.getFilterChain().addFirst("protocol", new ProtocolCodecFilter(RS2CodecFactory.GAME));
 					
