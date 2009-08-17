581d71b2dfb9b585bc674f84164a5988871aa346
Brett Russell <brettrussell@users.sourceforge.net>
2009-08-17 03:17:01 +0000

    This is the backbone of "Hyperite", the web frontend for Hyperion. Its setup will support ondemand module loading, as well as priority classes, templated page generation and much more. It will utilize a flatfile database so it can run on most systems.
    Keep in mind that this may yet be switched to Java to interact directly with the HTTP abilities of Hyperion's fileserver.
    
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@185 c48d7634-943c-464e-a392-e2f9434beeda

---
+++ web/class/main.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Copyright (c) 2009 Graham Edgecombe, Blake Beaupain and Brett Russell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+require("config.php");
+
+// initialize some constants
+define("dir_class","/class/");
+define("dir_module","/module/");
+define("dir_req","/req/");
+define("dir_util","/util/");
+
+/**
+ * Main class that initializes all other classes
+ *
+ */
+class Hyperite
+{
+	// __construct method is called when the object is initialized
+	public function __construct()
+	{
+		session_start();
+		$prio = explode(",",prio);
+		// load and initialize all priority classes or die on error
+		foreach($prio as $curr)
+		{
+			require(path_server.dir_class.dir_req.$curr.".php");
+			if(class_exists($curr))
+			{
+				$this->$curr = new $curr();
+				// bind that class to this object
+				$this->$curr->Hyperite = &$this;
+				// initialize if necessary
+				if(method_exists($this->$curr, 'init')) $this->$curr->init();
+			} else die("<strong>Fatal error:</strong> Required module class '$curr' failed to load.");
+		}
+	}
+	
+	// load a given module
+	public function loadModule($module)
+	{
+			require(path_server.dir_class.$module.".php");
+			if(class_exists($module))
+			{
+				$this->$module = new $module();
+				$this->$module->Hyperite = &$this;
+				if(method_exists($this->$module, 'init')) $this->$module->init();
+			} else echo("<b>Warning:</b> Could not initiate module ".$module);			
+	}
+	
+	// array search method
+	public function arrayMatch($needle, $array_stack)
+	{
+		foreach($array_stack as $curr)
+		{
+			if($curr == $needle) return true;
+		}
+		return false;
+	}
+}
+$hyperite = new Hyperite;
+?>
