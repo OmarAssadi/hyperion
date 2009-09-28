98399313ca9b4875e87d27b45103114c1242b273
Graham Edgecombe <graham@grahamedgecombe.com>
2009-09-28 18:28:36 +0000

    - Added emotes credits to vastico.
    git-svn-id: http://subversion.assembla.com/svn/hyperion_server@259 c48d7634-943c-464e-a392-e2f9434beeda

--- src/org/hyperion/rs2/model/Animation.java
+++ src/org/hyperion/rs2/model/Animation.java
@@ -6,6 +6,38 @@ package org.hyperion.rs2.model;
  *
  */
 public class Animation {
+
+	/**
+	 * Different animation constants.
+	 */
+	public final static Animation YES_EMOTE = create(855);
+	public final static Animation NO_EMOTE = create(856);
+	public final static Animation THINKING = create(857);
+	public final static Animation BOW = create(858);
+	public final static Animation ANGRY = create(859);
+	public final static Animation CRY = create(860);
+	public final static Animation LAUGH = create(861);
+	public final static Animation CHEER = create(862);
+	public final static Animation WAVE = create(863);
+	public final static Animation BECKON = create(864);
+	public final static Animation CLAP = create(865);
+	public final static Animation DANCE = create(866);
+	public final static Animation PANIC = create(2105);
+	public final static Animation JIG = create(2106);
+	public final static Animation SPIN = create(2107);
+	public final static Animation HEADBANG = create(2108);
+	public final static Animation JOYJUMP = create(2109);
+	public final static Animation RASPBERRY = create(2110);
+	public final static Animation YAWN = create(2111);
+	public final static Animation SALUTE = create(2112);
+	public final static Animation SHRUG = create(2113);
+	public final static Animation BLOW_KISS = create(1368);
+	public final static Animation GLASS_WALL = create(1128);
+	public final static Animation LEAN = create(1129);
+	public final static Animation CLIMB_ROPE = create(1130);
+	public final static Animation GLASS_BOX = create(1131);
+	public final static Animation GOBLIN_BOW = create(2127);
+	public final static Animation GOBLIN_DANCE = create(2128);
 	
 	/**
 	 * Creates an animation with no delay.
